16453c80cc54264db5cd99a87a51eeeef7ce1c15
Jeff Bryner <jeff@jeffbryner.com>
2018-10-13 21:16:34 -0700

    add form for editing

--- client/main.js
+++ client/main.js
@@ -1,23 +1,40 @@
+import { Blaze } from 'meteor/blaze';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import '/imports/collections.js';
 import './main.html';
+import './tags.html';
 
 Router.route('/', function () {
     this.render('osss');
   });
 
+Router.route('/solutions/:_id', function () {
+    var solution = solutions.findOne({_id: this.params._id});
+    console.log(solution);
+    this.render('solution_form', {data: solution});
+});
+
+Template.solutions.onCreated(function(){
+    this.autorun(() => {
+        this.subscribe('solutions');
+    });
+});
+
 Template.solutions.events({
     "click .solution-add": function(event,template){
-        $('#modal_solution_form').modal();
+        //create a new, empty solution
+        Session.set('solutionID',null);
+        newSolution=models.solution();
+        new_id=solutions.insert(newSolution);
+        //route to it for editing
+        Router.go('/solutions/' + Session.get('solutionID'));
+
     },
     "click .solution-edit": function(event,template){
-        $('#modal_solution_form').modal();
-        editSolution=models.solution();
-        editSolution=solutions.findOne({'_id':this._id});
-        template.find("#_id").value=editSolution._id;
-        template.find("#name").value=editSolution.name;
-        template.find("#description").value=editSolution.description;
+        Session.set('solutionID',this._id);
+        Router.go('/solutions/' + Session.get('solutionID'));
+
     },
     "click .solution-delete": function(e){
         id = $(e.target).attr('data-solutionid');
@@ -34,10 +51,30 @@ Template.solutions.helpers({
                             });
     }
 });
+Template.solution_form.onCreated(function (e,template) {
+    // Use this.subscribe inside onCreated callback
+    console.log('solution form created');
+    this.autorun(() => {
+        this.subscribe('solutions');
+        //this.subscribe('solution',Session.get('solutionID'));
+    });
+});
+
 
 Template.solution_form.events({
+    // input isn't enough since toolbar changes don't trigger
+    'input .solution_form': _.debounce(function(e,template){
+        //console.log('editor debounce input', template);
+        //window.tmpl=template;
+        this_solution=models.solution();
+        this_solution.name=template.find("#name").value;
+        this_solution.description=template.find("#description").value;
+        //this_solution.tags=template.data.tags;
+        this_solution.tags=_.without(_.pluck(template.find('.tag').childNodes,'nodeValue'),null)
+        //console.log('setting to:', this_solution);
+        solutions.update({ _id: template.data._id}, {$set: this_solution });
+    } , 500),
     "submit form": function(event, template) {
-        window.t=template;
         event.preventDefault();
         this_solution=models.solution();
 
@@ -49,5 +86,51 @@ Template.solution_form.events({
             solutions.update({ _id: template.find("#_id").value}, this_solution )
         }
         $('#modal_solution_form').modal('hide')
+    },
+    "dragover .tags": function(e){
+        e.preventDefault();   //allow the drag
+    },
+    "keyup .tagfilter":function(e,template){
+        //var letter_pressed = String.fromCharCode(e.keyCode);
+        console.log(template.find("#tagfilter").value);
+        Session.set('tagfilter',template.find("#tagfilter").value);
+
+    },
+    "drop .tags": function(e){
+        e.preventDefault();
+        tagtext = e.originalEvent.dataTransfer.getData("text/plain");
+        //e.target.textContent=droptag
+        //console.log(tagtext)
+        if ( Session.get('solutionID') ){
+            solutions.update({ _id: Session.get('solutionID')},
+             {$addToSet: {tags:tagtext}
+            });
+        }
+    },
+    "click .tagdelete": function(e){
+        tagtext = e.target.parentNode.firstChild.wholeText;
+        solutions.update(Session.get('solutionID'), {
+            $pull: {tags:tagtext}
+        });
+    },
+});
+
+
+Template.tags.helpers({
+    tags: function() {
+        return tags.find({tag:{$regex:'.*' +Session.get('tagfilter') + '.*',$options:'i'}},{limit:50});
+    }
+});
+
+Template.tags.events({
+    'dragstart .tag': function(e){
+        e.originalEvent.dataTransfer.setData("text/plain",this.tag);
+    },
+    'load': function(e, template){
+        template.find("#tagfilter").value=Session.get('tagfilter');
+    },
+    'click li': function(e,template){
+        Session.set('tagfilter',e.target.textContent);
+        template.find("#tagfilter").value=e.target.textContent;
     }
 });
