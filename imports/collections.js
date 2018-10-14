084d57a78067d532a77cae8eaa9c6d36a9c84748
Jeff Bryner <jeff@jeffbryner.com>
2018-10-13 21:16:52 -0700

    real publish

--- imports/collections.js
+++ imports/collections.js
@@ -4,14 +4,51 @@ import { Mongo } from 'meteor/mongo';
 Meteor.startup(() => {
     solutions = new Meteor.Collection("solutions");
     tags = new Meteor.Collection("tags");
-});
 
-models={
-    solution: function() {
-        return {
-            name:"",
-            description: "",
-            tags:[]
-        };
+    models={
+        solution: function() {
+            return {
+                name:"",
+                description: "",
+                tags:[]
+            };
+        }
     }
-}
+
+    if (Meteor.isServer) {
+
+        //Publish collections
+        Meteor.publish('solutions', function allSollutions(){
+            return solutions.find();
+        });
+        Meteor.publish('solution',function aSolution(id){
+            return solutions.findOne({_id:id});
+        })
+
+        Meteor.publish('tags', function(){
+            return tags.find();
+        })
+
+        // permissions, bare bones for now
+        solutions.allow({
+            insert: function (userId, doc) {
+                return (true);
+            },
+            update: function (userId, doc, fields, modifier) {
+                return (true);
+            },
+            remove: function (userId, doc) {
+                return (true)
+            }
+        });
+        tags.allow({});
+
+    }
+
+    if (Meteor.isClient) {
+        //client-side subscriptions to low volume collections
+        Meteor.subscribe("solutions");
+        Meteor.subscribe("tags");
+    };
+
+});
