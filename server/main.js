ab11a5d994a31d6f6a3af2744ae24c088fc6c60f
Jeff Bryner <jeff@jeffbryner.com>
2018-10-21 14:05:51 -0700

    add aggregation for pivot

--- server/main.js
+++ server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/meteor';
 import '/imports/collections.js';
+import { aggregate } from 'meteor/sakulstra:aggregate';
+
 
 Meteor.startup(() => {
     // code to run on server at startup
@@ -15,3 +18,26 @@ Meteor.startup(() => {
         });
     }
 });
+
+Meteor.methods({
+    'aggregateTags': aggregateTags
+});
+
+function aggregateTags() {
+    console.log("aggregating");
+    var pipeline = [
+      {$group: {_id: null, resTime: {$sum: "$resTime"}}}
+    ];
+    var pipeline = [
+
+        {"$match":{"tags":{"$exists":true}}},
+        {"$unwind": "$tags"},
+        {"$project": {"name": 1,
+                        "tags": 1,
+                        "_id": 0
+                        }}
+        ]
+    var result = solutions.aggregate(pipeline);
+    //console.log("Explain Report:", JSON.stringify(result[0]), null, 2);
+    return result;
+}
