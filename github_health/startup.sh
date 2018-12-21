45dec167c2735294f92f90463128edf563714632
Jeff Bryner <jeff@jeffbryner.com>
2018-12-21 14:14:19 -0800

    add initial startup script for python container

---
+++ github_health/startup.sh
@@ -0,0 +1,27 @@
+#! /bin/bash
+
+# fixup this python:3 docker image to run our cron
+apt-get update
+apt-get -y install cron rsyslog
+pip install -r requirements.txt
+
+/etc/init.d/rsyslog start
+/etc/init.d/cron start
+
+CRONTAB_ENTRY='
+* * * * * /usr/local/bin/python /opt/github_health/assess_health.py'
+
+cat << EOF > /var/spool/cron/crontabs/root
+${CRONTAB_ENTRY}
+EOF
+chmod 0600 /var/spool/cron/crontabs/root
+
+# log cron jobs
+cat << EOF >> /etc/default/cron
+EXTRA_OPTS='-L 5'
+EOF
+
+# make it go
+/etc/init.d/rsyslog restart
+/etc/init.d/cron restart
+
