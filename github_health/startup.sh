05c919624cd40f5e53126e80b66e85920d8a82ba
Jeff Bryner <jeff@jeffbryner.com>
2018-12-21 15:09:09 -0800

    allow for adding api key via env

--- github_health/startup.sh
+++ github_health/startup.sh
@@ -8,10 +8,17 @@ pip install -r requirements.txt
 /etc/init.d/rsyslog start
 /etc/init.d/cron start
 
+if [ -n "${GITHUB_API_KEY}" ]; then
+    echo 'adding github api key to cron'
+    cat <<- EOV > /var/spool/cron/crontabs/root
+GITHUB_API_KEY=${GITHUB_API_KEY}
+EOV
+fi
+
 CRONTAB_ENTRY='
 * * * * * /usr/local/bin/python /opt/github_health/assess_health.py'
 
-cat << EOF > /var/spool/cron/crontabs/root
+cat << EOF >> /var/spool/cron/crontabs/root
 ${CRONTAB_ENTRY}
 EOF
 chmod 0600 /var/spool/cron/crontabs/root
