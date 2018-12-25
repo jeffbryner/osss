a95ef4e6449694d9e34a728cb30b83ba7871214d
Jeff Bryner <jeff@jeffbryner.com>
2018-12-25 11:21:01 -0800

    set headers

--- server/main.js
+++ server/main.js
@@ -3,9 +3,15 @@ import { Mongo } from 'meteor/meteor';
 import '/imports/collections.js';
 import { aggregate } from 'meteor/sakulstra:aggregate';
 import { URL } from 'url';
+import { BrowserPolicy } from 'meteor/browser-policy';
 
 Meteor.startup(() => {
     // code to run on server at startup
+    //Modules.server.startup();
+    BrowserPolicy.content.disallowInlineScripts();
+    BrowserPolicy.content.disallowEval();
+    BrowserPolicy.content.disallowInlineStyles();
+    BrowserPolicy.framing.disallow();
     //update tags if missing:
     console.log("checking the tags");
     console.log('tags: ' + tags.find().count());
@@ -18,6 +24,15 @@ Meteor.startup(() => {
         });
     }
 });
+
+WebApp.rawConnectHandlers.use(function(req, res, next) {
+    // set the headers not included in the brower policy module
+    res.setHeader("Strict-Transport-Security", "max-age=63072000");
+    res.setHeader("X-Content-Type-Options","nosniff");
+    res.setHeader("X-XSS-Protection","1; mode=block");
+    res.setHeader("Referrer-Policy","no-referrer");
+    return next();
+  });
 
 Meteor.methods({
     'aggregateTags': aggregateTags
